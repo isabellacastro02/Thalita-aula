2a51210219ec2c3a9a96103662b04fdda4a9d36d
isabellacastro02 <isabella.mcastro02@gmail.com>
2025-08-18 10:57:04 -0300

    Add files via upload

--- Thalita-aula/src/App.jsx
+++ Thalita-aula/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import TenisNike from './assets/tenisnike.webp'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -16,6 +17,7 @@ function App() {
        <p className="desc"> Um lindo tenis da Nike</p>
        <p className="preco"> R$ 799,99 </p>
        <p className="avaliaco"> ★ ★ ★ ★ ☆ </p>
+       <img src={TenisNike} alt="" className="img-prod"/>
        </div>
 
        </section>
